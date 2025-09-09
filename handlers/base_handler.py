47340c26b555f8d550a9578eaa4389478f27c431
monodani <holigosts@gmail.com>
2025-09-09 10:12:23 +0900

    Update base_handler.py
    Signed-off-by: monodani <holigosts@gmail.com>

--- handlers/base_handler.py
+++ handlers/base_handler.py
@@ -36,7 +36,7 @@ class CentralOrchestrator:
         self.final_model = self.config.OPENAI_MODEL
         self.max_workers = 8
         self.handler_timeout = 30
-        self.chunks_per_handler = 8
+        self.chunks_per_handler = 10
 
         self.client = None
         if OPENAI_AVAILABLE and self.config.OPENAI_API_KEY:
