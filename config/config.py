4452bddc6511dc0ab945138b4afe2e1620541132
monodani <holigosts@gmail.com>
2025-09-09 11:06:41 +0900

    Update config.py
    Signed-off-by: monodani <holigosts@gmail.com>

--- config/config.py
+++ config/config.py
@@ -173,7 +173,7 @@ class AppConfig:
         "notice": {
             "type": "faiss", 
             "class": "NoticeHandler",
-            "description": "공지사항, 경남인재개발원 위치 및 시설 현황정보 및 전반 소개, 벼리 소개, 금주 교육일정 및 교육과정 안내"
+            "description": "공지사항, 경남인재개발원 위치 및 시설 현황정보 및 전반 소개, 역대 원장 현황정보, 원장님 인사말, 벼리 소개, 금주 교육일정 및 교육과정 안내"
         },
         "menu": {
             "type": "faiss", 
